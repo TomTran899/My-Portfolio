39170855ffb9f35c0fb24e81314496f75b01b126
Tungtran <viettunga9pct@gmail.com>
2022-04-04 23:00:31 +0700

    Color-darkMode-Animation

---
+++ js/script.js
@@ -0,0 +1,8 @@
+/* ========================================= Typing animation ========================================= */
+var typed = new Typed('.typing', {
+    strings: ['', 'Web Designer', 'Web Developer', 'Youtuber'],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
+
